df28406ec11874d5240b001a9e3e3ffa50b8bc7f
EmersonPaduan <emerson.paduan@gmail.com>
2024-09-17 11:09:28 -0300

    aula 05 - parte 3

--- aula05/server/Server.java
+++ aula05/server/Server.java
@@ -1,3 +1,24 @@
-public class Server {
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) {
+        final int PORT = 4567;
+        ServerSocket serverSocket = null;
+
+        try {
+            serverSocket = new ServerSocket(PORT);
+
+            while (true) {
+                System.out.println("Aguardando cliente...");
+                Socket client = serverSocket.accept();
+                
+                Attendant attendant = new Attendant(client);
+                attendant.start();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
 }
