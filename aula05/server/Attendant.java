d80f1726fbca55401eb975ad012bde52f0d451ba
EmersonPaduan <emerson.paduan@gmail.com>
2024-09-17 10:34:38 -0300

    aula 05 - parte 2

--- aula05/server/Attendant.java
+++ aula05/server/Attendant.java
@@ -1,3 +1,42 @@
-public class Attendant {
+import java.net.Socket;
+import java.util.HashMap;
 
+public class Attendant extends Thread {
+    private Socket client;
+    private Communication communication;
+
+    public Attendant(Socket socket) {
+        client = socket;
+        communication = new Communication(socket);
+    }
+
+    @Override
+    public void run() {
+        Translate translateReceived = (Translate) communication.receive();
+
+        String resp = translate(translateReceived);
+
+        Translate translateToSend;
+        if(resp != null) {
+            translateToSend = 
+                new Translate(resp, Status.SUCCESS);
+        } else {
+            translateToSend = 
+                new Translate(resp, Status.NOT_FOUND);
+        }
+
+        communication.send(translateToSend);
+    }
+
+    private String translate(Translate translate) {
+        HashMap<String, String> dictionaryIngPort = new HashMap<>();
+
+        dictionaryIngPort.put("red", "vermelho");
+        dictionaryIngPort.put("blue", "azul");
+        dictionaryIngPort.put("white", "branco");
+
+        String trad = dictionaryIngPort.get(translate.getWord());
+
+        return trad;
+    }
 }
